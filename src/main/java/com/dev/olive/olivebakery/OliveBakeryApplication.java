3674052ff35b3c4f8d7b6a5afb0337dbd2402601
ParkChunSo <cnsth123@gmail.com>
2019-02-07 16:36:47 +0900

    Initial commit

---
+++ src/main/java/com/dev/olive/olivebakery/OliveBakeryApplication.java
@@ -0,0 +1,14 @@
+package com.dev.olive.olivebakery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OliveBakeryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OliveBakeryApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/dev/olive/olivebakery/OliveBakeryApplicationTests.java
@@ -0,0 +1,17 @@
+package com.dev.olive.olivebakery;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OliveBakeryApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
